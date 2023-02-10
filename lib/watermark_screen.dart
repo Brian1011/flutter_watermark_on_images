b9cc5c60c370f290635ab62203d7834bccd2d975
Brian Mutinda <mutindabrian26@gmail.com>
2023-02-10 23:20:31 +0300

    add ui layout

--- lib/watermark_screen.dart
+++ lib/watermark_screen.dart
@@ -10,23 +10,42 @@ class WatermarkScreen extends StatefulWidget {
 }
 
 class _WatermarkScreenState extends State<WatermarkScreen> {
-  takePhoto() async {
-    File assetFile;
-    File _watermarkedImage;
-  }
+  late File assetFile;
+  File? watermarkedImage;
+
+  addWaterMarkToPhoto() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Add watermark to images")),
-      body: Container(
-        child: const Center(
-          child: IconButton(
-            icon: Icon(Icons.camera_alt_outlined, size: 40),
-            onPressed: null,
+        appBar: AppBar(title: const Text("Add watermark to images")),
+        body: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/rectangle_image.png',
+                height: 200,
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: addWaterMarkToPhoto,
+                child: const Text("Add water mark"),
+              ),
+              const SizedBox(height: 20),
+              watermarkedImage != null
+                  ? Image.file(
+                      watermarkedImage!,
+                      height: 200,
+                      width: 200,
+                    )
+                  : const SizedBox.shrink(),
+            ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
